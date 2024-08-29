3a5b78cb01cb949b7facb549e75c781b105875eb
Adarsh Sharma <a.sharma30double@gmail.com>
2024-08-29 21:04:39 +0530

    Created a post handler

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
-const PORT = 80;
+const PORT = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Get Success!");
+app.post("/", (req, res) => {
+  console.log(req);
+  res.sendStatus(200);
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
